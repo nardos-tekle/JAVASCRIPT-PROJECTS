cbc02a4ef2c6755c64e680915b27b703fa541ce2
Nardos-Tekle <nardostekl@gmail.com>
2024-12-04 23:38:07 -0800

    new files

---
+++ Basic JavaScript Projects/JS/main.js
@@ -0,0 +1,6 @@
+
+window.alert("Hello, world");
+document.write("Hello, World!");
+
+var Sent1 ="This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Hello, Universe!");
+
+// Created two variables and a line break variable.
+var Sent1 = "This is the beginning of the string";
+var Sent2 =  "and this is the end of the string";
+var z = "<br>";
+
+// Using the variables, this will concatenate both variables 
+document.write(Sent1 + " " +  Sent2);
+
+// Using the line break variable  it will create space
+// the expression can be on the next line. 
+document.write(z);
+
+//add the expression on the next line. 
+document.write(540 + 365);
+
+
