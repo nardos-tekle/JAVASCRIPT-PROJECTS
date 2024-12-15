c8e0a6f975c89c43ad091b5d0efadde350dadf9d
nardos-tekle <nardostekl@gmail.com>
2024-12-14 16:46:11 -0800

    Update main.js

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,12 +1,12 @@
 function my_dictionary(){
-    var Animal = {
+    var Animal = {      // Assigns variable to the key-pair collection. 
         Species:"Dog",
         Color:"Golden",
         Breed: "Corgie",
         Age: "Robot",
         Sound:"Bark",
     };
-    delete Animal.Sound;
-    document.getElementById("dictionary").innerHTML= Animal.Sound;
+    delete Animal.Sound; // this deletes the sound key-pair. 
+    document.getElementById("dictionary").innerHTML= Animal.Sound;  // Links the key-pair to the HTML element. However, the output will be undefined. 
 }
 
