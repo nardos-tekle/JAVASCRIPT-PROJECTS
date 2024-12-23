3a97562baf81d80195158883ba3460adb408f5be
Nardos-Tekle <nardostekl@gmail.com>
2024-12-23 04:58:37 -0800

    New folder

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,81 @@
+function function1(){
+    document.write(typeof "11" + 20);
+
+}
+
+function function2(){
+    document.write(4.87E10);    
+}
+
+function function3(){
+    document.write(-5.98E10);
+}
+
+function function4(){
+    document.write(55 > 52);
+}
+
+function function5(){
+    document.write(94 <= 84);
+}
+function function6(){
+    console.log(400 * 20);
+}
+
+function function7(){
+    document.write( 10 == 10);
+}
+
+function function8(){
+    document.write(4 == 10);
+}
+
+function function9(){
+    var X = 50;
+        Y = 50;
+
+    document.write(X===Y);
+}
+
+function function10(){
+    var Z = 100; 
+        W = "100";
+
+    document.write(Z===W);
+}
+
+function function11(){
+    var T = 34;
+        K = "Michelin";
+    document.write(T==K);
+}
+
+function function12(){
+    var D = 67;
+        F = 20;
+    document.write(D===F)
+}
+
+function function13(){
+    document.write(10 > 6 && 30 > 29);
+}
+
+function function14(){
+    document.write(20 > 50 && 10 > 4);
+}
+
+function function15(){
+    document.write(5 > 8 || 77 > 43);
+}
+
+function function16(){
+    document.write(6 > 10 || 33 > 50) 
+}
+
+function function17(){
+    document.getElementById("Not").innerHTML = ! (50 > 40); 
+}
+
+function function18(){
+    document.getElementById("not_func").innerHTML = ! (5 > 10);
+}
