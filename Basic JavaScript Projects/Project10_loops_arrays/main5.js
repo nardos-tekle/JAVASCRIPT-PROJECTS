8f15b60c5b0b4da6513cfbe95c75877782388f50
nardos-tekle <nardostekl@gmail.com>
2025-01-23 03:29:10 -0800

    Update main5.js

--- Basic JavaScript Projects/Project10_loops_arrays/main5.js
+++ Basic JavaScript Projects/Project10_loops_arrays/main5.js
@@ -1,6 +1,7 @@
+//calls the cat_pics function from HTML//
 function cat_pics(){
-    var cat = [];
-    cat[0] = "sleeping";
+    var cat = []; // declares the [] value under the cat variable//
+    cat[0] = "sleeping"; // each index within the cat object are declared with certain properties//.
     cat[1] = "playing";
     cat[2] = "eating";
     cat[3] = "purring";
