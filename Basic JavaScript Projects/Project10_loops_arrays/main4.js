f16a6added2136f44ff34e6b56755b494e476a7d
nardos-tekle <nardostekl@gmail.com>
2025-01-23 03:23:48 -0800

    Update main4.js

--- Basic JavaScript Projects/Project10_loops_arrays/main4.js
+++ Basic JavaScript Projects/Project10_loops_arrays/main4.js
@@ -1,10 +1,10 @@
-
+// declares the variable car with a block scope//
 let car = {
     make: "Toyota", 
     model: "Camry", 
     year: "2025", 
     color: "black",
-    description : function() {
+    description : function() { // Value description is given a function to return the following values under the variable car.description//
         return "the car is a" + this.year + this.color + this.make + this.model;
     }
 };
