5a7b1a0e608c613e966dfa9db41ccfd6b88c0dc1
nardos-tekle <nardostekl@gmail.com>
2025-01-23 02:55:14 -0800

    Update main.js

--- Basic JavaScript Projects/Project10_loops_arrays/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/main.js
@@ -1,9 +1,10 @@
-function Call_Loop(){
+// Creating variables//
     var Number = " ";
     var Z = 50;
-    while (Z < 100){
+    while (Z < 100){   //Like an If statment this will execute until it reaches to 100//
+function Call_Loop(){ // Calls the call_loop() from HTML to create an iteration counting from 50-100.// 
         Number += "<br>" + Z;
-        Z++;
+        Z++; //using ++ to add one each-time.//
         document.getElementById("Loop").innerHTML = Number;
     }
 }
